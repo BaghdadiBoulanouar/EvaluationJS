44c5bdfd1c41b7c4de13f8a6a6a438bc731e987a
Baghdadi <b.baghdadi93@gmail.com>
2020-10-12 19:47:13 +0200

    feat(exercice-2): complete function with progress element

--- exercice-2/index.js
+++ exercice-2/index.js
@@ -2,6 +2,16 @@ var drawBar = function drawBar(sum, nbr) {
     this.sum = sum; 
     this.nbr = nbr; 
   };
+  
+  drawBar.prototype.bar = function () {
+    var progressbar = document.createElement("progress");
+    progressbar.value = this.nbr;
+    progressbar.max = this.sum;
+    progressbar.style.width = "100%"; 
+    progressbar.style.height = "80px";
+    document.body.appendChild(progressbar);
+  };  
 
-
+  var tab = new drawBar(100, 30);
+  tab.bar();
   
