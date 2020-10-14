9ae7ebb77d1ee514c5dd55e6d4cc7cd1905ac82f
Baghdadi <b.baghdadi93@gmail.com>
2020-10-14 23:20:00 +0200

    feat(exercice-7): add JSDoc for ex-7

--- exercice-7/index.js
+++ exercice-7/index.js
@@ -11,6 +11,9 @@ computeNotes.prototype.run = function() {
     this.calculator();
 
 }
+/**
+ * Form for add numbers on a Array and calculate after
+ */
 computeNotes.prototype.form = function() {
     var f = document.createElement("form");
     //Entrée numéro form
@@ -36,7 +39,9 @@ computeNotes.prototype.form = function() {
     f.appendChild(s2);
     document.body.appendChild(f); //document.getElementsByTagName('body')[0].appendChild(f);
 }
-
+/**
+ * Add all numbers on the Array
+ */
 computeNotes.prototype.somme = function () { 
     var p = document.createElement('p');
     p.textContent = "Les notes rentrées : ";
@@ -51,7 +56,9 @@ computeNotes.prototype.somme = function () {
         console.log(Array + " " + Array.length)
     })
 }
-
+/**
+ * Calculate the average, sum of all number and divide with length
+ */
 computeNotes.prototype.calculator = function () { 
     var p2 = document.createElement('p');
     p2.textContent = "La moyenne est de : ";
