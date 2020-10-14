29b2482dbed26082ec6aa83206df4a7c7425854c
Baghdadi <b.baghdadi93@gmail.com>
2020-10-14 23:12:44 +0200

    feat(exercice-2): add JSDoc for ex-2

--- exercice-2/index.js
+++ exercice-2/index.js
@@ -1,8 +1,15 @@
+/** Crée Sum pour le maximum de la barre et nbr la valeur de remplissage
+ * @constructor
+ * @param  {} sum
+ * @param  {} nbr
+ */
 var drawBar = function drawBar(sum, nbr) {
     this.sum = sum; 
     this.nbr = nbr; 
   };
   
+  /** Fonction pour crée la progress bar
+   */
   drawBar.prototype.bar = function () {
     var progressbar = document.createElement("progress");
     progressbar.value = this.nbr;
